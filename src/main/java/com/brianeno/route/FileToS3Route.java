87738f00104474c5455036c5d357329d1b9282ba
agus-mac-031 <benochson@arisglobal.com>
2023-09-09 16:22:21 -0400

    updates

--- src/main/java/com/brianeno/route/FileToS3Route.java
+++ src/main/java/com/brianeno/route/FileToS3Route.java
@@ -4,27 +4,18 @@ import com.brianeno.service.FileProcessor;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws.s3.S3Constants;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FileToS3Route extends RouteBuilder implements ApplicationContextAware {
+public class FileToS3Route extends RouteBuilder {
 
     public FileToS3Route(@Autowired CamelContext context) {
         super(context);
     }
 
-
-    public void configures() throws Exception {
-        from("file:/tmp/camel-s3").to("file:/tmp");
-    }
-
     @Override
     public void configure() throws Exception {
-        //from(componentType + ":"+componentEp).routeId("camel-s3-example")
         from("{{from.component}}:{{from.endpoint}}").routeId("camelS3Route")
                 .log("${in.header.CamelFileNameOnly} to be uploaded to S3 {{awsS3BucketName}} bucket")
                 .log("Before content length: ${in.header.CamelFileLength}")
@@ -37,10 +28,5 @@ public class FileToS3Route extends RouteBuilder implements ApplicationContextAwa
                 .to("aws-s3://{{awsS3BucketName}}?deleteAfterWrite=false&region={{awsRegion}}&accessKey={{awsAccessKey}}&secretKey=RAW({{awsAccessSecretKey}})")
                 //optionally logging success message
                 .log("${in.header.CamelFileNameOnly} successfully uploaded to S3 {{awsS3BucketName}} bucket");
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("In aware");
     }
 }
